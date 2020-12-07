566e309ca3adc33c5d5e853e07b5b601891f0c89
Tim Beurskens <timbeurskens.97@gmail.com>
2020-12-07 17:01:34 +0100

    apply struct based AST parsing on the INI example

--- examples/ini/language.go
+++ examples/ini/language.go
@@ -3,29 +3,33 @@ package ini
 import . "goparselib"
 
 var (
-	eof        = CTerminal("#")
-	open       = CTerminal("\\[")
-	close      = CTerminal("\\]")
-	eq         = CTerminal("=")
-	str        = CTerminal("[a-z]*")
-	value      = Union{Float, str}
-	property   = Concat{Ident, BlankOpt, eq, BlankOpt, value, EOL}
-	properties = new(Symbol)
-	sectionId  = Concat{open, Ident, close, EOL}
-	section    = Concat{sectionId, R(properties)}
-	sections   = new(Symbol)
-	root       = Concat{R(properties), R(sections), eof}
+	parOpen        = CTerminal("\\[")
+	parClose       = CTerminal("\\]")
+	eq             = CTerminal("=")
+	str            = CTerminal("[a-zA-Z0-9_\\-\\ ,.;]*")
+	propertyident  = Concat{Ident, BlankOpt, eq, BlankOpt}
+	FloatProperty  = Concat{propertyident, Float}
+	StringProperty = Concat{propertyident, str}
+	Property       = Union{FloatProperty, StringProperty}
+	PropertyList   = new(Symbol)
+	sectionId      = Concat{parOpen, Ident, parClose}
+	Section        = Concat{sectionId, R(PropertyList)}
+	sections       = new(Symbol)
+	Root           = Concat{R(PropertyList), R(sections)}
+
+	// specify layout symbols for reduction
+	Layout = []Symbol{parOpen, parClose, eq, BlankOpt, Blank, nil, EOL}
 )
 
 func init() {
 	Define(sections, Union{
-		Concat{section, R(sections)},
+		Concat{Section, R(sections)},
 		Concat{EOL, R(sections)},
 		nil,
 	})
-	Define(properties, Union{
-		Concat{property, R(properties)},
-		Concat{EOL, R(properties)},
+	Define(PropertyList, Union{
+		Concat{Property, R(PropertyList)},
+		Concat{EOL, R(PropertyList)},
 		nil,
 	})
 }
--- examples/ini/tools.go
+++ examples/ini/tools.go
@@ -13,11 +13,11 @@ type Reader struct {
 }
 
 func New(input string) (Reader, error) {
-	tree, err := goparselib.ParseString(input, root)
+	tree, err := goparselib.ParseString(input, Root)
 	if err != nil {
 		return Reader{}, err
 	}
-	ast, err := tree.Reduce(nil, open, close, eof, eq, goparselib.Blank, goparselib.EOL, goparselib.BlankOpt)
+	ast, err := tree.Reduce(nil, parOpen, parClose, eq, goparselib.Blank, goparselib.EOL, goparselib.BlankOpt)
 	if err != nil {
 		return Reader{}, err
 	}
@@ -29,7 +29,7 @@ func New(input string) (Reader, error) {
 
 func (r Reader) Property(ident string) (string, error) {
 	prop, err := r.Model.Find(func(r1 goparselib.Node) bool {
-		if !reflect.DeepEqual(r1.Type, property) {
+		if !reflect.DeepEqual(r1.Type, Property) {
 			return false
 		}
 		_, err := r1.Find(func(r2 goparselib.Node) bool {
@@ -46,7 +46,7 @@ func (r Reader) Property(ident string) (string, error) {
 	}
 
 	v, err := prop.Find(func(r goparselib.Node) bool {
-		return reflect.DeepEqual(r.Type, value)
+		return reflect.DeepEqual(r.Type, goparselib.Float)
 	})
 
 	if err != nil {
@@ -64,7 +64,7 @@ func (r Reader) Property(ident string) (string, error) {
 
 func (r Reader) Section(ident string) (Reader, error) {
 	node, err := r.Model.Find(func(n goparselib.Node) bool {
-		if !reflect.DeepEqual(n.Type, section) {
+		if !reflect.DeepEqual(n.Type, Section) {
 			return false
 		}
 		_, err := n.Find(func(n2 goparselib.Node) bool {
