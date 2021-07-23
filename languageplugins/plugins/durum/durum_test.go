85fc2a5ff66e7a26ed095d29bac804149698b16d
Tim Beurskens <timbeurskens.97@gmail.com>
2021-07-23 14:32:10 +0200

    inline testcases for durum

--- languageplugins/plugins/durum/durum_test.go
+++ languageplugins/plugins/durum/durum_test.go
@@ -1,77 +1,49 @@
 package durum
 
 import (
-	"github.com/timbeurskens/goparselib/parser"
 	"testing"
+
+	"github.com/timbeurskens/goparselib/parser"
 
 	"github.com/timbeurskens/goparselib"
 )
 
-var definitions = map[string]string{
-	"basic integer": `def counter = int32_t`,
-	"parameter int": `def led = gpio_input 10`,
-}
-
-var events = map[string]string{
-	"timer tick": `event timer_tick = timer tick`,
-}
-
-var actions = map[string]string{
-	"no_param":     `action test {}`,
-	"single_param": `action test (a) {}`,
-}
-
-var states = map[string]string{
-	"basic":         `state initial {}`,
-	"basic_initial": `start state initial {}`,
-	"basic_eol": `start state initial {
+func TestStateDefinition(t *testing.T) {
+	t.Parallel()
+	DoTestInput(t, map[string]string{
+		"basic":         `state initial {}`,
+		"basic_initial": `start state initial {}`,
+		"basic_eol": `start state initial {
 }`,
-	"1 transition": `start state initial {
+		"1 transition": `start state initial {
 	on some_event goto next
 }`,
-	"multi transition": `start state initial {
+		"multi transition": `start state initial {
 	on some_event goto next
 	on other_event goto previous
 	on tick goto this_state
 }`,
-	"single_action_call": `start state initial {
+		"single_action_call": `start state initial {
 	on some_event goto next
 	do some_action
 }`,
-	"single_param_action_call": `start state initial {
+		"single_param_action_call": `start state initial {
 	on some_event goto next
 	do some_action (10)
 }`,
-	"multi_param_action_call": `start state initial {
+		"multi_param_action_call": `start state initial {
 	on some_event goto next
 	do some_action (a, 10, zozo)
 }`,
-}
-
-var full = map[string]string{
-	"basic": `def counter = int32_t 10
-
-event tick = counter overflow
-
-start state initial {
-	on tick goto next
-}
-
-state next {
-	on tick goto initial
-	do action
-}
-`,
-}
-
-func TestStateDefinition(t *testing.T) {
-	t.Parallel()
-	DoTestInput(t, states, State)
+	}, State)
 }
 
 func TestDefinitions(t *testing.T) {
 	t.Parallel()
-	DoTestInput(t, definitions, Definition)
+	DoTestInput(t, map[string]string{
+		"basic integer": `def counter = int32_t`,
+		"parameter int": `def led = gpio_input 10`,
+	}, Definition)
 }
 
 func TestFromFile(t *testing.T) {
@@ -84,25 +56,77 @@ func TestFromFile(t *testing.T) {
 
 func TestFullPartial(t *testing.T) {
 	t.Parallel()
-	DoTestInput(t, definitions, Root)
-	DoTestInput(t, events, Root)
-	DoTestInput(t, states, Root)
-	DoTestInput(t, actions, Root)
+	DoTestInput(t, map[string]string{
+		"basic integer": `def counter = int32_t`,
+		"parameter int": `def led = gpio_input 10`,
+	}, Root)
+	DoTestInput(t, map[string]string{
+		"timer tick": `event timer_tick = timer tick`,
+	}, Root)
+	DoTestInput(t, map[string]string{
+		"basic":         `state initial {}`,
+		"basic_initial": `start state initial {}`,
+		"basic_eol": `start state initial {
+}`,
+		"1 transition": `start state initial {
+	on some_event goto next
+}`,
+		"multi transition": `start state initial {
+	on some_event goto next
+	on other_event goto previous
+	on tick goto this_state
+}`,
+		"single_action_call": `start state initial {
+	on some_event goto next
+	do some_action
+}`,
+		"single_param_action_call": `start state initial {
+	on some_event goto next
+	do some_action (10)
+}`,
+		"multi_param_action_call": `start state initial {
+	on some_event goto next
+	do some_action (a, 10, zozo)
+}`,
+	}, Root)
+	DoTestInput(t, map[string]string{
+		"no_param":     `action test {}`,
+		"single_param": `action test (a) {}`,
+	}, Root)
 }
 
 func TestFull(t *testing.T) {
 	t.Parallel()
-	DoTestInput(t, full, Root)
+	DoTestInput(t, map[string]string{
+		"basic": `def counter = int32_t 10
+
+event tick = counter overflow
+
+start state initial {
+	on tick goto next
+}
+
+state next {
+	on tick goto initial
+	do action
+}
+`,
+	}, Root)
 }
 
 func TestEvents(t *testing.T) {
 	t.Parallel()
-	DoTestInput(t, events, Event)
+	DoTestInput(t, map[string]string{
+		"timer tick": `event timer_tick = timer tick`,
+	}, Event)
 }
 
 func TestActions(t *testing.T) {
 	t.Parallel()
-	DoTestInput(t, actions, Action)
+	DoTestInput(t, map[string]string{
+		"no_param":     `action test {}`,
+		"single_param": `action test (a) {}`,
+	}, Action)
 }
 
 func DoTestInput(t *testing.T, input map[string]string, symbol goparselib.Symbol) {
