fd49776edbf2906e0146e644aca808de966e44eb
Tim Beurskens <t.m.j.a.beurskens@student.tue.nl>
2021-10-29 18:18:26 +0200

    code formatting

--- definitions.go
+++ definitions.go
@@ -1,75 +1,75 @@
 package goparselib
 
 import (
-    "fmt"
-    "regexp"
+	"fmt"
+	"regexp"
 )
 
 type Symbol interface {
-    symbol()
-    fmt.Stringer
+	symbol()
+	fmt.Stringer
 }
 
 type Terminal struct {
-    Reg *regexp.Regexp
+	Reg *regexp.Regexp
 }
 
 func (t Terminal) String() string {
-    return fmt.Sprintf("T(%s)", t.Reg.String())
+	return fmt.Sprintf("T(%s)", t.Reg.String())
 }
 
 func (t Terminal) symbol() {
-    panic("implement me")
+	panic("implement me")
 }
 
 type Union []Symbol
 
 func (u Union) String() string {
-    return fmt.Sprintf("U(%d)", len(u))
+	return fmt.Sprintf("U(%d)", len(u))
 }
 
 func (u Union) symbol() {
-    panic("implement me")
+	panic("implement me")
 }
 
 type Concat []Symbol
 
 func (c Concat) String() string {
-    return fmt.Sprintf("C(%d)", len(c))
+	return fmt.Sprintf("C(%d)", len(c))
 }
 
 func (c Concat) symbol() {
-    panic("implement me")
+	panic("implement me")
 }
 
 type Reference struct {
-    R *Symbol
+	R *Symbol
 }
 
 func (r Reference) String() string {
-    return fmt.Sprintf("R(%d)", r.R)
+	return fmt.Sprintf("R(%d)", r.R)
 }
 
 func (r Reference) symbol() {
-    panic("implement me")
+	panic("implement me")
 }
 
 func R(s *Symbol) Reference {
-    return Reference{s}
+	return Reference{s}
 }
 
 func CTerminal(expr string) Terminal {
-    return Terminal{regexp.MustCompilePOSIX(expr)}
+	return Terminal{regexp.MustCompilePOSIX(expr)}
 }
 
 func Define(s *Symbol, s2 Symbol) {
-    *s = s2
+	*s = s2
 }
 
 type Node struct {
-    Start    int64  `json:"start"`
-    Size     int64  `json:"size"`
-    Contents string `json:"contents"`
-    Type     Symbol `json:"-"`
-    Children []Node `json:"children"`
+	Start    int64  `json:"start"`
+	Size     int64  `json:"size"`
+	Contents string `json:"contents"`
+	Type     Symbol `json:"-"`
+	Children []Node `json:"children"`
 }
